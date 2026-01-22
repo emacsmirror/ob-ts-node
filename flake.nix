4e031b90fca5f9621479cedd7e4934037d01418c
Alexandr Timchenko <atimchenko92@gmail.com>
2026-01-23 00:26:01 +0100

    ci: prepare ci/cd environment

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "ob-ts-node development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              emacs
+              bun
+              nodejs
+              typescript
+              python3
+              python3Packages.pip
+              gnumake
+            ];
+
+            shellHook = ''
+              export PATH="$PWD/node_modules/.bin:$PATH"
+              echo "ob-ts-node development environment"
+              echo "Available commands: make format, make test, make lint, make build, make clean, make checkdoc"
+            '';
+          };
+        }
+      );
+    };
+}
